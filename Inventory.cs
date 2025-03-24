a0958f9a6048d4bb1efd2399e179a6a8ca8a317b
Justin Gresnich <justingresnich@gmail.com>
2025-03-24 06:00:37 -0700

    added item and inv class

---
+++ Inventory.cs
@@ -0,0 +1,5 @@
+class Inventory 
+{
+//fields
+private int maxweight;
+}
---
+++ src/Item.cs
@@ -0,0 +1,13 @@
+class Items
+{
+    //fields
+    public int Weight {get;}
+    public string Description {get;}
+
+    //constructor
+    public Items(int weight, string description)
+{
+    Weight = weight;
+    Description = description;  
+    }
+}
